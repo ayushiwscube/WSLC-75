fd4d7382086e7cbd0043a83ba5f1b6708cd24e71
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-09-26 18:56:54 +0530

    Add files via upload

---
+++ 26092022.py
@@ -0,0 +1,82 @@
+a = "HARRY Potter"
+b = "123#45#78#9"
+
+c = "charlie and the chocolate factory"
+
+#length function
+print(len(a))
+print(len(c))
+
+#count function
+print(a.count("r"))
+print(b.count("#"))
+
+#lower function
+print(a.lower())
+
+#upper function
+print(a.upper())
+
+#swapcase function
+print(a.swapcase())
+
+#capitalize function
+print(c.capitalize())
+
+#casefold function
+print(a.casefold())
+
+
+#chocolate
+#and
+
+# print(c[16:25])
+# print(c[-25:-22])
+
+
+
+
+# word = input("enter a word to check: ")
+#
+# temp = word[::-1]
+# if word == temp:
+#     print("it is palindrome")
+
+
+
+
+
+
+# print(b[::2])
+# print(b[::-1])
+
+# print(a[0:5])
+# print(a[6:9])
+# print(a[:5])
+# print(a[6:])
+
+# print(a[11])
+# print(a[-1])
+
+#Homework
+#check if an integer is palindrome
+#check if two strings are of equal length
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in range(0,6):
+#     for j in range(5,i,-1):
+#         print ("", end = " ")
+#     for k in range(i):
+#         print (j, end = " ")
+#     print()
