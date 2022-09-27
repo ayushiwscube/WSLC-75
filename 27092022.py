29b5f162b0333b4010d7b463c3a671174100bf18
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-09-27 19:02:24 +0530

    Add files via upload

---
+++ 27092022.py
@@ -0,0 +1,136 @@
+#Homework
+#enter a string and obtain all the vowels from the string
+
+#write a paragrph of string 4-5 lines.
+#Get all capitalised elements from the paragraph.
+#count the number of "." in your paragraph
+#count the total number of words in your paragraph
+
+
+
+
+
+
+
+
+
+# grocery = ["banana","apple","kiwi","pineapple"]
+#
+# for i in grocery:
+#     print(i)
+#
+#
+# for ind, i in enumerate(grocery):
+#     print (ind,i)
+
+
+
+
+
+# print(grocery)
+# print(type(grocery))
+#
+# print (grocery[-2])
+# print(len(grocery))
+#
+# print(grocery[:2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = "Charlie And The Chocolate Factory 5"
+# b = "hello123"
+# c = "HarryPotter"
+# d = "123456"
+# e = " "
+# f = "#OOTD#TBT#YOLO"
+#
+# name = "ayushi"
+# age = 24
+# print ("my name is",name)
+# print (f'my name is {name} and my age is {age}')
+
+
+
+
+
+
+# print (a.istitle())
+
+
+
+
+
+# print(b)
+# print(b.strip("3"))
+
+# print(b.split("o"))
+
+# print (e.isspace())
+
+# print(a.isalnum())
+# print(b.isalnum())
+# print(c.isalpha())
+# print(d.isdigit())
+# print (c.islower())
+
+
+
+
+
+# print (a.find("c"))
+# print (a.index("c"))
+
+
+
+# number = int(input("enter a number here: "))
+# rev = 0
+# temp = number
+#
+# while temp != 0:
+#     digit = temp%10
+#     rev = (rev * 10)+digit
+#     temp = temp//10
+#
+# print(rev)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
