b8fecbc205c982e2801112d898deef05166a523d
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-07 19:03:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+#l = [10,30,50,20,40]
+
+#find the length of the list without using length function
+# count = 0
+# for i in l:
+#     count += 1
+#
+# print (count)
+
+
+
+
+
+#find the sum of all the numbers present in the list
+#l = [10,30,50,20,40]
+# total = sum[l]
+# print(total)
+
+# total = 0
+# for i in l:
+#     total +=i
+#
+# print (total)
+
+
+
+#find the product of all the number present in the list
+# l = [10,30,50,20,40]
+#
+# product = 1
+# for i in l:
+#     product*=i
+#
+# print(product)
+
+
+#swap first and the last element from the list
+#Method 1
+# l = [10,30,50,20,40]
+# l[0],l[4] = l[4],l[0]
+# print(l)
+
+#Method 2
+# print(l)
+# temp = l[0]
+# l[0] = l[4]
+# print (l)
+# l[4] = temp
+# print(l)
+
+#clone a list
+#l = [10,30,50,20,40]
+#method 1
+# a = []
+#
+# for i in l:
+#     if i > 30:
+#         a.append(i)
+# print (a)
+
+#Method 2
+# a = [i for i in l if i >30]
+# print(a)
+
+
+#count the number of duplicate elements(40) in the list without using count function
+# l = [10,30,50,20,40,40,30,40,20,10,30]
+#
+# count = 0
+#
+# for i in l:
+#     if i == 40:
+#         count+=1
+#
+# print(count)
+
+
+
+
+
+
+
+#find the largest number from the list
+
+# l = [10,30,50,20,40]
+#
+# l.sort()
+# print(l)
+#
+# print("the largest number in the list is",l[-1])
+
+#find the smallest number from the list
+
+#print("the smallest number in the list is",l[0])
