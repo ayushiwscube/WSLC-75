5e66ee7ad63578f9da7b830fdd7b4c0e60459693
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-12 18:59:40 +0530

    Create 1210222.py

---
+++ 1210222.py
@@ -0,0 +1,226 @@
+char = "a-zA-Z1234567890!@#$%^&*"
+
+
+
+
+
+
+# import math
+# print(math.floor(2.25))
+# print(math.ceil(2.25))
+# print(math.pi*3)
+# print((22/7)*3)
+# print(3.14*3)
+# print(abs(-12))
+# print(max(12,34,67))
+# print(math.pow(2,5))
+# print(math.sqrt(49))
+
+
+
+
+
+
+
+
+# import time
+#
+# start = time.time()
+#
+# num = int(int())
+#
+# end = time.time()
+#
+# print(end-start)
+
+
+
+
+# import time
+#
+#
+# print("hello")
+# time.sleep(5)
+# print("world")
+
+
+
+
+
+
+
+# from datetime import date
+#
+# a = date(23,45,57)
+# print(a)
+
+
+
+# import datetime
+#
+# a = datetime.datetime.now()
+# print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import xyz
+#
+# xyz.add_num(12,45)
+
+
+
+
+
+
+# from xyz import school
+
+
+
+
+
+# import xyz
+#
+# xyz.hello("john")
+#
+# a = xyz.school["Age"]
+# print(a)
+
+
+
+
+
+
+
+#number guessing game
+
+#computer guess (1-100)
+# 78
+#ask user to guess the number
+
+# 50
+#incorrect guess
+#your number is smaller than the computer's number'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+# words = ["jupiter","smartphone","saturn","oneplus","telephone"]
+# chances = 3
+# word = random.choice(words)
+# jumble = "".join(random.sample(word,len(word)))
+#
+# print("~~~~~ game of jumbled words ~~~~~")
+# while chances !=0:
+#     print("the word is",jumble)
+#     guess = input("Guess the word: ")
+#     if guess == word:
+#         print("correct answer")
+#         break
+#     else:
+#         chances -= 1
+#         print("remaining chances are",chances)
+# else:
+#     print("you lose")
+#     print("the correct word is",word)
+#
+# print("thank you for playing")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+# word = "jupiter"
+# jumble = "".join(random.sample(word,len(word)))
+# print(jumble)
+
+
+
+
+
+# import random
+# a = [1,2,3,4,5,6]
+# random.shuffle(a)
+# print(a)
+
+
+
+
+
+# import random
+#
+# num = random.randint(1,100)
+# print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+#
+# coin = [1,2,3,4,5,6]
+#
+# toss = random.choice(coin)
+# print(toss)
