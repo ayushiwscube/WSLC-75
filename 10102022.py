9060c0511426b8f423fa9942009e2a31f1b3bdad
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-18 19:01:40 +0530

    Create 10102022.py

---
+++ 10102022.py
@@ -0,0 +1,64 @@
+class Smartphone:
+    def __init__(self):
+        print("these are the details of the phone")
+
+    def phone(abc,ram,camera):
+        print("the phone has a ram of",ram,"and the camera of ", camera)
+        
+    def phone2(self):
+        print("the phone is of nokia")
+
+a = Smartphone()
+a.phone2()
+# b = Smartphone()
+# b.phone("8 GB", "30MP")
+
+
+
+
+
+
+
+
+
+
+
+# class hello:
+#     name = "john"
+#     age = 23
+#     print(age)
+#
+# a = hello()
+# b = hello()
+# print(a.name)
+
+
+
+
+
+
+
+
+
+
+
+
+# import yfinance as yf
+#
+# msft = yf.Ticker("MSFT")
+# data =(msft.info)
+#
+# cp = data["currentPrice"]
+# print(cp)
+
+
+
+# import random
+# char = "1234567890"
+#
+# number = int(input('enter the length of OTP: '))
+# ps = ""
+# for i in range(0, number):
+#     a = random.choice(char)
+#     ps += a
+# print(ps)
