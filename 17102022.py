d041ccdd9d254a5e029f06d69ec189a1d9b86ca4
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-19 13:04:10 +0530

    Create 17102022.py

---
+++ 17102022.py
@@ -0,0 +1,106 @@
+# l = [None]*10
+# print(len(l))
+
+
+
+# import datetime
+#
+# a = datetime.date(1997,10,14)
+# print(a.strftime("a"))
+# print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# f = open("demo.txt","a")
+# while True:
+#      name = input("enter name: ")
+#      f.write("\nName: ")
+#      f.write(name)
+#
+#      subject = input('enter subject: ')
+#      f.write("\nSubject: ")
+#      f.write(subject)
+#
+#      marks = input("enter marks: ")
+#      f.write("\nMarks: ")
+#      f.write(marks)
+#      f.write("\n")
+#
+#      repeat = input("repeat again?")
+#      if repeat == "no":
+#           break
+#
+# f.close()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#write mode
+# f = open("demo.txt","w")
+#
+# f.write("Ooops!! deleted")
+#
+# f.close()
+
+
+
+
+
+
+
+
+
+# #Appending mode
+#
+# f = open("demo1.txt","a")
+# #f.write("\nthank you")
+# text ="\nbye"
+#
+# for i in range(0,5):
+#      f.write(text)
+# f.close()
+
+
+
+
+
+
+
+
+
+
+#reading Mode
+# f = open("demo.txt","r")
+#
+# # print(f.read(10))
+# # print(f.readline())
+# # print(f.readline())
+# # data =(f.readlines())
+# #
+# # for i in data:
+# #      print(i)
+#
+# f.close()
